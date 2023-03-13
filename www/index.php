080f3a6489bfa968bf8ee7144a071fe27807eaf0
Thando Mini <tzone85@gmail.com>
2023-03-13 20:56:53 +0200

    accessed another api using an api key for authentication to get the data from it

--- www/index.php
+++ www/index.php
@@ -8,12 +8,15 @@
     // another way of doing the above in a curl_setopt_array where one can set up multiple curl options at once
 
     curl_setopt_array($ch, [
-        CURLOPT_URL => "https://randomuser.me/api",
+        CURLOPT_URL => "https://api.openweathermap.org/data/2.5/weather?g=London&appid=9d15fee35ff5df920ff7f135bec06c6d",
         CURLOPT_RETURNTRANSFER => true
     ]);
 
     $response = curl_exec($ch);
 
+    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
+
+    echo $status_code."\n";
 
     echo $response."\n";
