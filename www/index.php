0688e19fb638466b95a282e19ea601b7d95b2bd2
Thando Mini <tzone85@gmail.com>
2023-03-13 21:30:49 +0200

    accessed a new api, one that required an access key to be specified within it's headers. Unsplash api

--- www/index.php
+++ www/index.php
@@ -7,9 +7,13 @@
 
     // another way of doing the above in a curl_setopt_array where one can set up multiple curl options at once
 
+    $headers = [
+        "Authorization: Client-ID 8oJnUkpnVPmC5tLDttLG7ELCyeNHGOM7LCFaO1sUn3A"
+    ];
     curl_setopt_array($ch, [
-        CURLOPT_URL => "https://api.openweathermap.org/data/2.5/weather?g=London&appid=9d15fee35ff5df920ff7f135bec06c6d",
-        CURLOPT_RETURNTRANSFER => true
+        CURLOPT_URL => "https://api.unsplash.com/photos/random",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => $headers
     ]);
 
     $response = curl_exec($ch);
