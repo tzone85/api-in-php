7019f13cec37d4c320b83a6dcabe577c26558ad5
Thando Mini <tzone85@gmail.com>
2023-03-14 05:34:42 +0200

    explored headers even further, added status_codes, conten_type and content_length

--- www/index.php
+++ www/index.php
@@ -13,14 +13,23 @@
     curl_setopt_array($ch, [
         CURLOPT_URL => "https://api.unsplash.com/photos/random",
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => $headers
+        CURLOPT_HTTPHEADER => $headers,
+        CURLOPT_HEADER => true              // response header
     ]);
 
     $response = curl_exec($ch);
 
     $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $content_type = curl_getinfo($ch,CURLINFO_CONTENT_TYPE);
+    $content_length = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+
+
     curl_close($ch);
 
+
     echo $status_code."\n";
+    echo $status_code."\n";
+    echo $content_type."\n";
+    echo $content_length."\n";
 
     echo $response."\n";
