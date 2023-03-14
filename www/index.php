b8ae46058a33fda5b70b464df3e5bfe91dace0a4
Thando Mini <tzone85@gmail.com>
2023-03-14 21:49:55 +0200

    made a few changes to show other ways of making a POSt request onto the github repo

--- www/index.php
+++ www/index.php
@@ -8,18 +8,24 @@
     // another way of doing the above in a curl_setopt_array where one can set up multiple curl options at once
 
     $headers = [
-        "Authorization: token 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TT"
+        "Authorization: token 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TT"
         //"User-Agent: tzone85"
     ];
 
+    $payload = json_encode([
+        "name" => "Created from API",
+        "description" => "An example API-created repo"
+    ]);
 
 
     curl_setopt_array($ch, [
-        CURLOPT_URL => "https://api.github.com/user/starred/httpie/httpie",
+        CURLOPT_URL => "https://api.github.com/user/repos",
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_HTTPHEADER => $headers,
         CURLOPT_USERAGENT => "tzone85",
-        CURLOPT_CUSTOMREQUEST => "PUT"       // DELETE or something else but must be in upper case
+        // CURLOPT_CUSTOMREQUEST => "POST",      // DELETE or something else but must be in upper case
+        // CURLOPT_POST => true,                 // These two are the same.
+        CURLOPT_POSTFIELDS => $payload
     ]);
 
     $response = curl_exec($ch);
