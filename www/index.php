1d217b791374f13c9167b55dd118dce9315f30a4
Thando Mini <tzone85@gmail.com>
2023-03-14 22:06:15 +0200

    returning all the info partaining to a specific gist

--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@
 $ch = curl_init();
 
 curl_setopt_array($ch, [
-    CURLOPT_URL => "https://api.github.com/gists",
+    CURLOPT_URL => "https://api.github.com/gists/9d92339aa433405afd3f457c4ae0fe1b",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_USERAGENT => "tzone85"
 
@@ -15,6 +15,10 @@ curl_close($ch);
 
 $data = json_decode($response, true);
 
+print_r($data);
+
+/*
 foreach ($data as $gist) {
     echo $gist["id"]. "-".$gist["description"]."\n";
 }
+*/
